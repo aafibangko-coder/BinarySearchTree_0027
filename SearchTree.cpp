d14d81717349686b5b112b5146e282101d76f2bc
aafibangko-coder <aafibangko@gmail.com>
2026-06-11 16:21:10 +0700

    menambahkan metode insert untuk kelas BinaryTree

--- SearchTree.cpp
+++ SearchTree.cpp
@@ -28,4 +28,34 @@ public:
         root = NULL; // Iitalizing ROOT to NULL
     }
 
+    // Insert a node in the binary search tree
+    void insert(string element)
+    {
+        Node *newNode = new Node(element, NULL, NULL);
+
+        newNode->info = element;
+        newNode->leftchild = NULL;
+        newNode->rightchild = NULL;
+
+        Node *parent = NULL;
+        Node *currentNode = NULL;
+
+        search(element, parent, currentNode);
+
+        if (parent == NULL)
+        {
+            root = newNode;
+            return;
+        }
+
+        if (element < parent->info)
+        {
+            parent->leftchild = newNode;
+        }
+        else if (element > parent->info)
+        {
+            parent->rightchild = newNode;
+        }
+    }
+
     
